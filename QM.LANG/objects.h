7353893e96d25a40c386c235b949872e65773ebe
Marshall Lucas <mlucas67@gmail.com>
2013-04-12 20:30:23 -0500

    Combined

---
+++ QM.LANG/objects.h
@@ -0,0 +1,9 @@
+$define new object
+$define protected public
+$define endif end
+$define endlocate end
+$define false 0
+$define true 1
+deffun isObject(obj) local
+deffun className(obj) local
+deffun hasMethod(obj, method) local
